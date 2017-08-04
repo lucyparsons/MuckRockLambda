2c5bf65a09fd435ccf38d4a77ee9d8abd168d84c
Freddy Martinez <fremartinez@expedia.com>
2017-08-04 12:25:17 -0500

    add instructions and sample lambda

---
+++ MuckRock_lambda.py
@@ -0,0 +1,55 @@
+import json
+import requests
+from datetime import datetime
+import dateutil.relativedelta
+import os 
+
+username = os.environ['my_username']
+passphrase = os.environ['my_passphrase']
+api_url = 'https://www.muckrock.com/api_v1/'
+
+
+response = requests.post('https://www.muckrock.com/api_v1/token-auth/',
+                         data={'username': username, 'password': passphrase })
+data = response.json()
+token = data['token']
+:print('Token received from MR successfully')
+
+
+def get_headers(token=None):
+    if token:
+        return {
+        'Authorization': 'Token %s' %token,
+        'content-type': 'application/json'
+        }
+    else:
+        return {'content-type': 'application/json'}
+
+
+def lambda_handler(event, context): 
+    print("Received event")
+    # first get today's month then subtract one month to grab previous
+
+    s = str(datetime.now())[0:10]
+    d = datetime.strptime(s, "%Y-%m-%d")
+    d2 = d - dateutil.relativedelta.relativedelta(months=1)
+    # strip day of the week + other timedate objects
+    d2 = str(d2)[0:7]
+
+    # use test agencies from MuckRock
+    foia_doc = json.dumps({
+        'jurisdiction': 10,
+        'agency': 248,
+        'title': 'Last Month\'s 1505 checks',
+        'document_request':
+            'For the year and month of ' + d2 +
+            ', I am requesting a list of all 1505 checks expenditures issues by the Bureau of Organized Crime. '
+            'I am requesting the full list of checks spent by the BoC during the full month of ' + d2 + '. ',
+        })
+                                                                                                                        
+    header = get_headers(token)
+    foia_url = api_url+'foia/'
+    r = requests.post(foia_url, data=foia_doc, headers=header)
+    print(r.text)
+    print(r.status_code)
+    print('All done')
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# MuckRockLambda
-A short AWS Lambda to automate our FOIA requests
+# MuckRock Lambda
+
+A short AWS Lambda to automate our FOIA requests.
+
+# What you need to know
+
+A working knowledge of AWS lambda and python
+
+# Deployments
+
+Bundle up the python `requests` library into your lamba zip file before you upload it to AWS. For this to happen, in the top level directory (the one with your `.py` files), execute a `pip install -t . requests`. You can leave out this README from the zip. 
+
+# Environment Variables
+
+This script depends on your lambda setting your MuckRock username and password as environment variables. This is probably not ideal, so consider using a KMS for storing your keys.
+
+# Contibute
+
+Ping us on [Twitter](https://twitter.com/lucyparsonslabs) or send us a PR on here. 
